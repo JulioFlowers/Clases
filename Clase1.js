4c13648aee165233a8d0ead5f4ff02a4b805cfcc
Julio Flowers <julio_alfonso2000@hotmail.com>
2021-02-16 04:12:09 -0600

    avance clase

--- Clase1.js
+++ Clase1.js
@@ -22,6 +22,58 @@ var prompt = require('prompt-sync')()
 
 /* Los tenemos de 2 tipos
 
-Lo primitivos: */
+Lo primitivos: Son aquellos tipos de datos por denominarlos
+basicos, no son objetos y no tienen propiedades, ademas son 
+inmutables i,e. solo se pueden reasignar de manera directa
 
+En JS tenemos 6
+- Undifined : Es lit nada y solo aparece cuando definimos 
+una variable y no le asignamos ningun valor.
+- Numbers.
+- Strings.
+- Boolean. 
+- Symbol: El tipo Symbol nos permite obtener valores que no 
+pueden volver a ser creados, es decir, son identificadores
+ únicos e inmutables.
+ - Big Int Sirve para representar con mayor precision numeros
+ enteros.
 
+ Veamos algunos ejemplos */
+
+var Perro
+console.log(typeof Perro)
+
+var estudiantes = 8
+typeof estudiantes
+
+var nombre = "Aquiles Meles Parto"
+console.log(typeof nombre)
+
+var elhigoesfruta = false
+console.log(typeof elhigoesfruta)
+
+/* y los tipo complejo.
+
+ Estos se dividen a su vez en 2 las funciones que veremos despues
+ y tipo Objeto.
+
+ Los tipo objeto: un valor en memoria al que podemos acceder 
+ por un identificador, tenemos 2 casos particulares para estos en JS.
+
+ - Arreglos: aquellos objetos  similares a una lista cuyo prototipo 
+ proporciona métodos para efectuar operaciones de recorrido y de mutación.
+ - Null: Undefined para Objetos
+ 
+ veamos ejemplos*/
+
+var vacio = null
+console.log(typeof vacio)
+
+var listacompras = ["huevos", 12, "camioneta", true]
+console.log(typeof listacompras)
+
+var datos = { nombre: "Lic. Valeriano", edad: 23, casado: false }
+console.log(typeof datos)
+
+var bañar = function bañar(perro){conole.log("voy a bañar a " + perro + " que es mi perro fav")}
+console.log(typeof bañar)
