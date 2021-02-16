fa2e82c7b6625d135d862292820a57f0f68c8199
Julio Flowers <julio_alfonso2000@hotmail.com>
2021-02-16 02:49:45 -0600

    Update Clase1.js

--- Clase1.js
+++ Clase1.js
@@ -1,11 +1,27 @@
-
 //Clase n°1 Feb. 16. 2021
 
-var prompt = require('prompt-sync')();
+"use strict"; // nos permite trabajar con una estructura mas formal
 
- console.log("Hola mundo");
+//modulo para poder introducir datos de manera asincrona
+var prompt = require('prompt-sync')()
 
- var numero = 23;
- console.log("numero");
- numero = prompt('Como te llamas? ');
- console.log(numero);
+//muestra en consola el Mensaje "Hola mundo"
+ console.log("Hola mundo")
+
+ /*La finamilidad de este tramo de codigo es poder mostrar que 
+ JavaScript (JS) es un lenguaje de Tipado dinamico i,e. las variables
+ no tienen un tipo de dato establecido y pueden cambiar, ademas 
+ se muestra como hacer un comentario de varias lineas.*/
+
+ var numero = 23
+ console.log(numero)
+ numero = prompt('Como te llamas? ')
+ console.log(numero)
+
+//Veamos tipos de datos.
+
+/* Los tenemos de 2 tipos
+
+Lo primitivos: */
+
+
