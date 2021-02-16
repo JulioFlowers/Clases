c258ab6cd7631b4b82bbf09de51c8288d2192a4d
Julio Flowers <julio_alfonso2000@hotmail.com>
2021-02-16 02:48:59 -0600

    clase 1

---
+++ Clase1.js
@@ -0,0 +1,11 @@
+
+//Clase n°1 Feb. 16. 2021
+
+var prompt = require('prompt-sync')();
+
+ console.log("Hola mundo");
+
+ var numero = 23;
+ console.log("numero");
+ numero = prompt('Como te llamas? ');
+ console.log(numero);
