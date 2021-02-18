c3a9de796a9b2808a0e49dde550df8bbb61c2f37
Julio Flowers <julio_alfonso2000@hotmail.com>
2021-02-17 21:29:48 -0600

    sugerencia del dia

---
+++ index.js
@@ -0,0 +1,16 @@
+"use strict"
+
+const Prompt = require('prompt-sync')()
+
+var x = "pedro";
+
+var edad = 8;
+
+function añitosdepedro(años){      
+    console.log("Pedrito ahora tiene: ", edad+=años)
+}
+
+añitosdepedro(8)
+console.log("Pedrito tiene: " + edad)
+
+console.log("Dia especial es una cancion de Sahkira")
